4210aa974a4185dc17b0d8cabe96edbf64f04a79
Splashscar <felipemendieta389@gmail.com>
2026-06-19 17:49:34 -0500

    archivos

--- frontend/src/app/services/eventos.ts
+++ frontend/src/app/services/eventos.ts
@@ -6,12 +6,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosService {
 
-  private apiUrl = 'http://127.0.0.1:8000/api/eventos/';
+  private apiUrl = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) {}
 
   obtenerEventos() {
-    return this.http.get(this.apiUrl);
+    return this.http.get(`${this.apiUrl}/eventos/`);
   }
+<<<<<<< HEAD
 }
 
+=======
+
+  login(datos: any) {
+    return this.http.post(`${this.apiUrl}/auth/login/`, datos);
+  }
+
+  register(datos: any) {
+    return this.http.post(`${this.apiUrl}/auth/register/`, datos);
+  }
+
+}
+>>>>>>> a180e0d58f15b2d88392bc8dcaae00372fd94311
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyAf1l1vPwc_eL5ojmzvOP-jFQrn0OhnX4k",
+        authDomain: "advaih.firebaseapp.com",
+        projectId: "advaih",
+        storageBucket: "advaih.firebasestorage.app",
+        messagingSenderId: "635806688897",
+        appId: "1:635806688897:web:fa9fb2b226980dd4f98f80"
+    }
+};
