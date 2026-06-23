d50e0c6f91f9a143c85af67754cb227131978a45
David Anzola <davidanzola014@gmail.com>
2026-06-23 13:43:23 -0500

    Agregar CRUD de eventos en home y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
 import { HomeComponent } from './pages/home/home';
+import { CrudEventos } from "./pages/crud-eventos/crud-eventos"
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    {path: 'crud-eventos', component: CrudEventos}
 ];
